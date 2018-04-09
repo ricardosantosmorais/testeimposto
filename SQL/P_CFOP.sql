0dee2b55e78884540f89c1b40d273323ca901bca
Ricardo Morais <ricardo.morais@gmail.com>
2018-04-09 18:19:56 -0300

    Add files via upload

--- SQL/P_CFOP.sql
+++ SQL/P_CFOP.sql
@@ -1,64 +1,34 @@
-USE [Teste]
-GO
-IF OBJECT_ID('dbo.P_NOTA_FISCAL') IS NOT NULL
+IF OBJECT_ID('dbo.P_CFOP') IS NOT NULL
 BEGIN
-    DROP PROCEDURE dbo.P_NOTA_FISCAL
-    IF OBJECT_ID('dbo.P_NOTA_FISCAL') IS NOT NULL
-        PRINT '<<< FALHA APAGANDO A PROCEDURE dbo.P_NOTA_FISCAL >>>'
+    DROP PROCEDURE dbo.P_CFOP
+    IF OBJECT_ID('dbo.P_CFOP') IS NOT NULL
+        PRINT '<<< FALHA APAGANDO A PROCEDURE dbo.P_CFOP >>>'
     ELSE
-        PRINT '<<< PROCEDURE dbo.P_NOTA_FISCAL APAGADA >>>'
+        PRINT '<<< PROCEDURE dbo.P_CFOP APAGADA >>>'
 END
 go
 SET QUOTED_IDENTIFIER ON
 GO
 SET NOCOUNT ON 
 GO 
-CREATE PROCEDURE dbo.P_NOTA_FISCAL 
-(
-	@pId int OUTPUT,
-	@pNumeroNotaFiscal int,
-	@pSerie int,
-	@pNomeCliente varchar(50),
-	@pEstadoDestino varchar(50),
-	@pEstadoOrigem varchar(50)
-)
+CREATE PROCEDURE dbo.P_CFOP 
 AS
 BEGIN
-	IF (@pId = 0)
-	BEGIN 
-		INSERT INTO [dbo].[NotaFiscal]
-           ([NumeroNotaFiscal]
-           ,[Serie]
-           ,[NomeCliente]
-           ,[EstadoDestino]
-           ,[EstadoOrigem])
-		VALUES
-           (@pNumeroNotaFiscal
-           ,@pSerie
-           ,@pNomeCliente
-           ,@pEstadoDestino
-           ,@pEstadoOrigem)
-
-		SET @pId = @@IDENTITY
-	END
-	ELSE
-	BEGIN
-		UPDATE [dbo].[NotaFiscal]
-		SET [NumeroNotaFiscal] = @pNumeroNotaFiscal
-		  ,[Serie] = @pSerie
-		  ,[NomeCliente] = @pNomeCliente
-		  ,[EstadoDestino] = @pEstadoDestino
-		  ,[EstadoOrigem] = @pEstadoOrigem
-		WHERE Id = @pId
-	END	    
+	SELECT Cfop
+	, SUM(BaseIcms) AS TotalBaseICMS
+	, SUM(ValorIcms) AS TotalValorICMS
+	, SUM(BaseIpi) AS TotalBaseIPI
+	, SUM(ValorIpi) AS TotalValorIPI
+	FROM NotaFiscalItem
+	GROUP BY Cfop
 END
 GO
-GRANT EXECUTE ON dbo.P_NOTA_FISCAL TO [public]
+GRANT EXECUTE ON dbo.P_CFOP TO [public]
 go
 IF OBJECT_ID('dbo.P_NOTA_FISCAL') IS NOT NULL
-    PRINT '<<< PROCEDURE dbo.P_NOTA_FISCAL CRIADA >>>'
+    PRINT '<<< PROCEDURE dbo.P_CFOP CRIADA >>>'
 ELSE
-    PRINT '<<< FALHA NA CRIACAO DA PROCEDURE dbo.P_NOTA_FISCAL >>>'
+    PRINT '<<< FALHA NA CRIACAO DA PROCEDURE dbo.P_CFOP >>>'
 go
 
 
